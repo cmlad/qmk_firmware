15b63df449a15fd531bf9fd7601616f0a3f19d46
Christian Mladenov <cmlad@cmlad.com>
2021-11-04 17:00:01 -0700

    Move modifiers

--- keyboards/moonlander/keymaps/cmlad/keymap.c
+++ keyboards/moonlander/keymaps/cmlad/keymap.c
@@ -48,9 +48,9 @@ enum custom_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_moonlander(
     KC_GRAVE,         KC_1,          KC_2,          KC_3,         KC_4,          KC_5,             KC_6,                                                        KC_7,                  KC_8,           KC_9,          KC_0,             KC_MINUS,        KC_EQUAL,           KC_BSPACE,
-    KC_TAB,           KC_Q,          KC_W,          KC_E,         KC_R,          KC_T,             KC_LANGLEBR_RANGLEBR,                                        KC_BSLASH,             KC_Y,           KC_U,          KC_I,             KC_O,            KC_P,               KC_SLASH,
-    LT(1,KC_ESCAPE),  LCTL_T(KC_A),  LALT_T(KC_S),  LGUI_T(KC_D), LSFT_T(KC_F),  KC_G,             KC_NO,                                                       KC_LBRACKET_RBRACKET,  KC_H,           LSFT_T(KC_J),  LGUI_T(KC_K),     LALT_T(KC_L),    LCTL_T(KC_SCOLON),  LT(2,KC_QUOTE),
-    KC_NO,            KC_Z,          KC_X,          KC_C,         KC_V,          KC_B,                                                                                                 KC_N,           KC_M,          KC_COMMA_LBRACE,  KC_DOT_RBRACE,   KC_NO,              KC_NO,
+    KC_TAB,           KC_Q,          KC_W,          KC_E,         KC_R,          KC_T,             KC_NO,                                                       KC_NO,                 KC_Y,           KC_U,          KC_I,             KC_O,            KC_P,               KC_BSLASH,
+    LT(1,KC_ESCAPE),  LCTL_T(KC_A),  LALT_T(KC_S),  LGUI_T(KC_D), LSFT_T(KC_F),  LCAG_T(KC_G),     KC_NO,                                                       KC_LBRACKET_RBRACKET,  LCAG_T(KC_H),   LSFT_T(KC_J),  LGUI_T(KC_K),     LALT_T(KC_L),    LCTL_T(KC_SCOLON),  LT(2,KC_QUOTE),
+    KC_NO,            KC_Z,          KC_X,          KC_C,         KC_V,          KC_B,                                                                                                 KC_N,           KC_M,          KC_COMMA_LBRACE,  KC_DOT_RBRACE,   KC_SLASH,           KC_LANGLEBR_RANGLEBR,
     KC_NO,            KC_NO,         KC_NO,         MO(1),        KC_NO,         KC_AUDIO_VOL_UP,                                                                                      KC_AUDIO_MUTE,  KC_NO,         MO(2),            KC_NO,           KC_NO,              KC_NO,
                                                                                  LT(2,KC_SPACE),   KC_NO,                 KC_AUDIO_VOL_DOWN,          KC_NO,    KC_ENTER,              LT(1,KC_SPACE)
   ),
